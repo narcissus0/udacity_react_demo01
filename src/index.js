0a576b622917d827954eb93de98e6f70c6fe0e7c
narcissus0 <jiangwy000@gmail.com>
2019-06-18 20:58:24 +0800

    createElement
    使用createElement

--- src/index.js
+++ src/index.js
@@ -3,14 +3,44 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+
+// 方式一
+// const element = React.createElement('ol',{className:'red'},
+//     React.createElement('li',null,'a'),
+//     React.createElement('li',null,'b')
+// )
+
+// 方式二
+const people = [
+  { name: "Tyler" },
+  {
+    name: "Karen"
+  },
+  {
+    name: "Richard"
+  }
+];
+const element = React.createElement(
+  "ol",
+  { className: "red" },
+  people.map(person =>
+    React.createElement(
+      "li",
+      { className: "red", key: person.name },
+      person.name
+    )
+  )
+);
+
+console.log(element);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(element, rootElement);
