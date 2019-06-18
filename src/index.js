ed52065193a4be604765fa52ef35681c31fe1278
narcissus0 <jiangwy000@gmail.com>
2019-06-18 22:58:54 +0800

    jsx转化
    Rearct.createElement转化成jsx格式

--- src/index.js
+++ src/index.js
@@ -2,45 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+// const people = [
+//   { name: "Tyler" },
+//   {
+//     name: "Karen"
+//   },
+//   {
+//     name: "Richard"
+//   }
+// ];
+// const element = (
+//   <ol>
+//     {people.map(person => (
+//       <li>{person.name}</li>
+//     ))}
+//   </ol>
+// );
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
-
-// 方式一
-// const element = React.createElement('ol',{className:'red'},
-//     React.createElement('li',null,'a'),
-//     React.createElement('li',null,'b')
-// )
-
-// 方式二
-const people = [
-  { name: "Tyler" },
-  {
-    name: "Karen"
-  },
-  {
-    name: "Richard"
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: "Tyler" },
+      {
+        name: "Karen"
+      },
+      {
+        name: "Richard"
+      }
+    ];
+    return (
+      <ol>
+        {people.map(person => (
+          <li key={person.name} className="red">
+            {person.name}
+          </li>
+        ))}
+      </ol>
+    );
   }
-];
-const element = React.createElement(
-  "ol",
-  { className: "red" },
-  people.map(person =>
-    React.createElement(
-      "li",
-      { className: "red", key: person.name },
-      person.name
-    )
-  )
-);
+}
 
-console.log(element);
+// console.log(element);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(element, rootElement);
+ReactDOM.render(<ContactList />, rootElement);
